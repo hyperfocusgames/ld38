43ffdd7df259116fb414485ecd91f79b23b09c3d
Tanner Grehawick <tgrehawi@gmail.com>
2017-04-22 22:41:12 -0400

    planets vary in size

--- Assets/Scripts/Planet.cs
+++ Assets/Scripts/Planet.cs
@@ -6,9 +6,12 @@ public class Planet : MonoBehaviour {
 
 	public Transform surfaceModel;
 	public float radius = 1;
+	public float radiusVariance = 0.5f;
 	public Color backgroundColor = Color.black;
 
 	void Awake() {
+		radius += Random.Range(-radiusVariance, radiusVariance);
+		surfaceModel.localScale = Vector3.one * radius * 2;
 	}
 
 	void OnValidate() {
