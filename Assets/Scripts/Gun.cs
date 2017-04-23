b85d2b37babbcab135e5dfa3509ba7f0e330dfac
Cethin <cethin@live.com>
2017-04-22 20:09:56 -0400

    Projectile physics started

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -7,18 +7,16 @@ public class Gun : MonoBehaviour
 	public void activate(GameObject projectile, float speed, int damage, bool damPlayer, bool damEnemy)
 	{
 		GameObject go = Instantiate(projectile, transform.position, Quaternion.Euler(transform.forward));
-		Damager dam = go.AddComponent<Damager>();
-		SurfaceEntity entity = go.AddComponent<SurfaceEntity>();
-		entity.hoverHeight = GetComponentInParent<SurfaceEntity>().hoverHeight;
-
-		// ***** None of these work *****
-		entity.body.AddForce(speed * transform.up, ForceMode.Impulse);
-		entity.body.AddForce(speed * transform.forward, ForceMode.Impulse);
-		entity.body.AddForce(speed * transform.right, ForceMode.Impulse);
-		// ******************************
-
+		
+		Damager dam = GetComponent<Damager>();
 		dam.damage = damage;
 		dam.damPlayer = damPlayer;
 		dam.damEnemy = damEnemy;
+
+		SurfaceEntity entity = GetComponent<SurfaceEntity>();
+		entity.hoverHeight = GetComponentInParent<SurfaceEntity>().hoverHeight;
+
+		GetComponent<Projectile>().force = speed * transform.up;
+
 	}
 }
