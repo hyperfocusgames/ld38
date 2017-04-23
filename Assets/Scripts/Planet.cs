b8b26a5e45fb37f340a5a82d559c90f04e638bdc
Tanner Grehawick <tgrehawi@gmail.com>
2017-04-23 17:14:44 -0400

    sky gradient

--- Assets/Scripts/Planet.cs
+++ Assets/Scripts/Planet.cs
@@ -7,7 +7,7 @@ public class Planet : MonoBehaviour {
 	public Transform surfaceModel;
 	public float radius = 1;
 	public float radiusVariance = 0.5f;
-	public Color backgroundColor = Color.black;
+	public Gradient sky;
 
 	void Awake() {
 		radius += Random.Range(-radiusVariance, radiusVariance);
@@ -18,6 +18,14 @@ public class Planet : MonoBehaviour {
 		if (surfaceModel) {
 			surfaceModel.localScale = Vector3.one * radius * 2;
 		}
+	}
+
+	void Update() {
+		CameraRig rig = CameraRig.instance;
+		Sun sun = Sun.instance;
+		float day = Vector3.Dot(rig.transform.forward.normalized, sun.transform.forward.normalized);
+		day = (day + 1) / 2;
+		rig.skyColor = sky.Evaluate(day);
 	}
 
 }
