6568f4d7905f39941fb30acdd1e16b3b6f1b70d5
Andrew Van Buren <avanbur2@masonlive.gmu.edu>
2017-04-23 00:47:26 -0400

    Added intro screen and scroll text.
    Also added audio into the project.

---
+++ Assets/Scripts/ChangeSceneAfterX.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeSceneAfterX : MonoBehaviour {
+
+	public string scene = "";
+	public float seconds = 70;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(Wait());
+	}
+
+	IEnumerator Wait() {
+		yield return new WaitForSeconds(seconds);
+		SceneManager.LoadScene(scene);
+	}
+}
