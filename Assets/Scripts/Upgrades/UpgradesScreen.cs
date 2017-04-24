bcbec9be721d8e65700562480180f91c3231a9f1
Veloxiac <matthewkelly95@gmail.com>
2017-04-24 01:46:03 -0400

    UpgradeHighlightOnAwake

--- Assets/Scripts/Upgrades/UpgradesScreen.cs
+++ Assets/Scripts/Upgrades/UpgradesScreen.cs
@@ -9,15 +9,18 @@ public class UpgradesScreen : MonoBehaviour
 	public Button button;
 
 	private Upgrade[] upgrades;
+	private UnityEngine.EventSystems.EventSystem eventSystem;
 
 	void Awake ()
 	{
 		upgrades = new Upgrade[UpgradesDeck.numUpgradesToDraw];
+		eventSystem = GameObject.Find ("EventSystem").GetComponent<UnityEngine.EventSystems.EventSystem> ();
 		if(button != null)
 		{
 			for(int i = 0; i < UpgradesDeck.numUpgradesToDraw; i++)
 			{
 				Button b = Instantiate(button, Vector3.zero, Quaternion.identity);
+				if(i == 0) eventSystem.SetSelectedGameObject(b.gameObject);
 				b.transform.SetParent(transform);
 				Upgrade u = UpgradesDeck.draw();
 				b.GetComponent<UpgradeButton>().Upgrade = u;
