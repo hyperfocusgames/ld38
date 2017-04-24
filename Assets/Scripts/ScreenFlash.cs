60cb4eebc5295e37922c7a2d03e04eb79426cff5
Tanner Grehawick <tgrehawi@gmail.com>
2017-04-24 01:27:15 -0400

    warpgate flash is now generic, can be used for anything

--- Assets/Scripts/ScreenFlash.cs
+++ Assets/Scripts/ScreenFlash.cs
@@ -3,20 +3,23 @@ using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
 
-public class WarpFlash : SingletonBehaviour<WarpFlash> {
+public class ScreenFlash : SingletonBehaviour<ScreenFlash> {
 
-	public Color color;
-	public float fadeDuration = 2;
+	Color color;
+	float duration = 0;
 
 	Image image;
 	float t;
 
 	void Awake() {
 		image = GetComponent<Image>();
-		t = fadeDuration;
+		color = Color.clear;
+		t = 0;
 	}
 
-	public void Flash() {
+	public void Flash(Color color, float duration) {
+		this.color = color;
+		this.duration = duration;
 		t = 0;
 	}
 
@@ -24,7 +27,7 @@ public class WarpFlash : SingletonBehaviour<WarpFlash> {
 		t += Time.deltaTime;
 		Color transparent = color;
 		transparent.a = 0;
-		image.color = Color.Lerp(color, transparent, t / fadeDuration);
+		image.color = Color.Lerp(color, transparent, t / duration);
 	}
 
 
